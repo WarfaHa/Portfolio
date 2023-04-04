323a645107c0ca50bd63e9d0b1becb68c15b5e83
Warfa <warfahassan35@yahoo.com>
2023-04-03 21:01:10 -0400

    setup react components

---
+++ src/app.js
@@ -0,0 +1,24 @@
+//Todo
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
+
+function App() {
+    return (
+      <div>
+        <Nav />
+        <About />
+        <Portfolio/>
+        <Contact />
+        <Contact />
+        <Resume />
+        <Footer />
+      </div>
+    );
+  }
+  
+  export default App;
+  
---
+++ src/components/Portfolio.js
@@ -0,0 +1,12 @@
+//Todo
+import React from 'react';
+import './App.css';
+
+function Portfolio() {
+    return(
+<div></div>
+);
+}
+
+
+export default Portfolio;
